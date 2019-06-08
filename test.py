749bba194bbc2983a6da1f80f27e68716b0ee296
JeffMacaluso <macaluso.jeff@gmail.com>
2019-06-07 17:31:29 -0700

    Added tests

--- test.py
+++ test.py
@@ -4,11 +4,59 @@ import pandas as pd
 
 import mlcookbook
 
-data = pd.DataFrame({'x1': [1, 5, 30000],
-                     'x2': [3, 10, 9],
-                     'y': [1, 9, 11]})
-print(data)
+from sklearn.datasets import load_boston
 
+boston_data = load_boston()
+data = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
+data['Target'] = boston_data.target
+
+print()
+print('----------------------------------------------')
+print('Testing EDA')
+print()
+
+print('-----------')
+print('Percent Missing Test')
 mlcookbook.eda.percent_missing(data)
+print()
 
+print('-----------')
+print('IQR outliers')
+outliers = mlcookbook.eda.iqr_indices_of_outliers(data.iloc[:, 0])
+print(outliers)
+print()
+
+print('-----------')
+print('Z score outliers')
+outliers = mlcookbook.eda.z_score_indices_of_outliers(data.iloc[:, 0])
+print(outliers)
+print()
+
+print('-----------')
+print('Percentile outliers')
+outliers = mlcookbook.eda.percentile_indices_of_outliers(data.iloc[:, 0])
+print(outliers)
+print()
+
+print('-----------')
+print('Ellipses outliers')
+outliers = mlcookbook.eda.ellipses_indices_of_outliers(data)
+print(outliers)
+print()
+
+print('-----------')
+print('Isolation Forest outliers')
+outliers = mlcookbook.eda.isolation_forest_indices_of_outliers(data)
+print(outliers)
+print()
+
+print('-----------')
+print('One class SVM outliers')
+outliers = mlcookbook.eda.one_class_svm_indices_of_outliers(data)
+print(outliers)
+print()
+
+print('-----------')
+print('Outlier Report')
 mlcookbook.eda.outlier_report(data)
+print()
