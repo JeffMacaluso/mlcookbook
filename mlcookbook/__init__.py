bd45c144f2dd7328c3a1cae909abdfc51824a6a3
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2019-06-24 14:25:45 -0700

    Added function to print library versions and system hardware

--- mlcookbook/__init__.py
+++ mlcookbook/__init__.py
@@ -4,3 +4,23 @@ import mlcookbook.misc
 import mlcookbook.nlp
 import mlcookbook.plot
 import mlcookbook.process
+
+def diagnostics():
+    '''
+    Reports the current date/time, package versions, and machine hardware
+    
+    TODO: Make this work dynamically with imported libraries
+    '''
+    import sys
+    import os
+    import time
+    import numpy as np
+    import pandas as pd
+    import sklearn
+    print(time.strftime('%Y/%m/%d %H:%M'))
+    print('OS:', sys.platform)
+    print('CPU Cores:', os.cpu_count())
+    print('Python:', sys.version)
+    print('NumPy:', np.__version__)
+    print('Pandas:', pd.__version__)
+    print('Scikit-Learn:', sklearn.__version__)
